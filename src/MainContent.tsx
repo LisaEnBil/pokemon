dd9a0a831d3041773ba412f15a92aea09c067052
Lisa Gillfrost <lisa.gillfrost@cross-solutions.com>
2023-06-22 08:54:55 +0200

    show modal when card is clicked

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -1,26 +1,36 @@
 import './MainContent.css';
 import { Header } from "./Header";
-import { PokemonList } from "./PokemonList";
-import { useState } from 'react';
+import { PokemonList, PokemonListType } from "./PokemonList";
+import { useEffect, useState } from 'react';
 import { Modal } from './Modal';
 
 
 export function MainContent(){
     const [isClicked, setIsClicked] = useState<boolean>(false);
+    const [pokemonList, setPokemonList] = useState<PokemonListType[]>();
+  
+    useEffect(() => {
+
+        fetch("https://pokeapi.co/api/v2/pokemon?limit=150")
+        .then((response) => response.json())
+        .then((data) => setPokemonList(data.results))
+        .catch((error) => console.log(error))
+      
+    }, [pokemonList]
+    );
+
+    if (!pokemonList){
+      return <>NOOOO!</>
+    }
 
 
     const handleClick = (isClicked: boolean) => {
         setIsClicked(isClicked);
       }
-      console.log(isClicked);
     return <div className="main-content">
-        <Header />
+        <Header /> 
         <PokemonList handleClick={handleClick} />
-        {
-            isClicked &&
-            <Modal name={"pelle"} handleClick={handleClick} />    
-        }
+        {isClicked && <Modal name={"pelle"} handleClick={handleClick} /> }   
     </div>
 }
-
-
+    
